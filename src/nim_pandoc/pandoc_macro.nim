781d32d855c9d0a9afafeb7884165cbd7eb005f3
pysan3 <pysan3@gmail.com>
2023-04-25 06:04:44 +0900

    delete unnecessary output

--- src/nim_pandoc/pandoc_macro.nim
+++ src/nim_pandoc/pandoc_macro.nim
@@ -6,14 +6,12 @@ import strformat
 macro refObjectJsonHook*(baseType: untyped, conds: untyped): untyped =
   var body = newSeq[string](0)
   body.add(&"proc toJsonHook*(self: {repr(baseType)}): JsonNode =")
-  body.add("""  echo self.t""")
   body.add("  case self.t:")
   for rcond in conds:
     body.add("    of " & rcond[0].mapIt(repr(it)).join(", ") & ":")
     body.add(&"      result = toJson(cast[{repr(rcond[1]).strip}](self))")
   body.add("    else: discard\n")
   body.add(&"proc fromJsonHook*(self: var {repr(baseType)}, o: JsonNode, opt = Joptions()) =")
-  body.add("""  echo o{"t"}.getStr()""")
   body.add("""  case o{"t"}.getStr():""")
   for rcond in conds:
     body.add("    of " & rcond[0].mapIt(repr(it)).join(", ") & ":")
